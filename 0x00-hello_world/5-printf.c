6217b50e54461fbd0f7ca9b843a9bd0446d13c3a
astewul alemu <loloalex1992@gmail.com>
2022-04-10 15:09:15 +0300

    commiting 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
  **/
 int main(void)
 {
-	printf("\n with proper grammer, but the outcome is a piece of art,");
+	printf("\n with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
