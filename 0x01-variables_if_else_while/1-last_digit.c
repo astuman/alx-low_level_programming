c6e027e78754a74689cc78770b0fa8ab5ec30abc
astuman <101445231+astuman@users.noreply.github.com>
2022-04-10 22:31:37 +0300

    commiting last

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Entry
+ *main - Entry
  * 
- * description - prints a number
- * last digit is 0, greater than 5 or less than 6
- * Return always 0
+ *description - prints a number
+ *last digit is 0, greater than 5 or less than 6
+ *Return always 0
  **/
 int main(void)
 {
