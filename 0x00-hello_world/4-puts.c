ad67a3a7514968f7326dc022c497670c635edffe
astewul alemu <loloalex1992@gmail.com>
2022-04-10 14:40:20 +0300

    commiting 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /*
- * * here is the main entry
- * *return: 0 after printing the function
- * */
+ ** here is the main entry
+ **return: 0 after printing the function
+ **/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
