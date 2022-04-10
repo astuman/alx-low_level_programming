d0cc8ede65a5a0b75771dd63d41b6ac17d89886e
astewul alemu <loloalex1992@gmail.com>
2022-04-10 13:50:20 +0300

    commiting 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include <stdio.h>
+/*here is the main entry
+ *
+ * returns after printing the function
+ * 
+ * */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
