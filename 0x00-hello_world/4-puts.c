0b780e693e8ac8e0a9bb2a2b397aec60a106b23f
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-10 17:10:36 +0300

    puts.c commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main function: - entry point
- *My hello, puts task
- *return: 0 after printing the function
- */
+ * main function: - entry point
+ * My hello, puts task
+ * return: 0 after printing the function
+ **/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
