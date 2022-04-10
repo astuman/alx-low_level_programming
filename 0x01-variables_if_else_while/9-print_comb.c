4a949e1044912a16221d907271313d22c9b989a7
astuman <101445231+astuman@users.noreply.github.com>
2022-04-11 00:27:10 +0300

    commiting last

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+*main -Entry
+*
+*Return: Always 0 (Success or correct)
+*/
+int main(void)
+
+{
+int number;
+
+for (number = 48; number <= 57; number++)
+{
+putchar(number);
+if (number == 57)
+{
+	break;
+}
+putchar(':')
+putchar( )
+}
+
+putchar ('\n');
+
+return (0);
+}
