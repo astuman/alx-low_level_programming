25815d596f4d69790f15f3ebe6767b49af9999ee
astuman <101445231+astuman@users.noreply.github.com>
2022-04-11 00:09:02 +0300

    commiting last

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+*main -Entry
+*
+*Return: Always 0 (Success or correct)
+*/
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 122; alphabet <= 97; alphabet--)
+{
+putchar(alphabet);
+}
+
+putchar ('\n');
+
+return (0);
+}
