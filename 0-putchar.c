b08021233f2092b393fe659df0c6e648c833a79e
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-12 15:46:53 +0300

    commit my 0x02 function

---
+++ 0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main-main block
+ * Description: print putchar in new line
+ * Return: the program returns 0
+ */
+inta main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
