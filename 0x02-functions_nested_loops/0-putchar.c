3a88c37cde8724bae8cf6e2da2cf2df67f91fa8b
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-13 11:48:45 +0300

    commit nested loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,22 @@
 #include "main.h"
 
+
 /**
- * main-main block
- * Description: print putchar in new line
- * Return: the program returns 0
+ * main - Entry point
+ *
+ * Return: nothing return void
  */
 int main(void)
 {
-char ermias[] = "_putchar";
-int c;
-for (c = 0; c < 8; c++)
-{
-_putchar(ermias[c]);
-}
-putchar('\n');
+_putchar('_');
+_putchar('p')'
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
 return (0);
 }
