63fcb88cc9533f87ef5d780181c20173c10a92d4
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-13 12:14:06 +0300

    commit nested loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print alphabetic - print alphabetic lowercase
+*
+*Return: always 0
+*/
+void print_alphabetic(void)
+{
+char alphabet;
+int i;
+for (i = 0; i <= 10; i++){
+
+for (alphabetic = 'a'; alphabteci <= 'z'; alphabtic++)
+{
+_putchar(alphabtic);
+}}
+
+}
