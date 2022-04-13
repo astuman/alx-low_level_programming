d64d047954fc88b5db988d754f553a89e8a65b8e
astuman <101445231+astuman@users.noreply.github.com>
2022-04-13 12:36:20 +0300

    commit 10x alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,20 +1,28 @@
+
 #include "main.h"
 
 /**
- * print_alphabet - print alphabet lowercase
+ * print_alphabet_x10 - print alphabet
  *
  * Return: Always 0.
  */
-
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-char alphabet;
+int alphabet;
+int count;
 
-for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+
+count = 0;
+while (count < 10)
+{
+for (alphabet = 'a' ; alphabet <= 'z'; alphabet++)
 {
 _putchar(alphabet);
 }
 
+count++;
 _putchar('\n');
+}
+
 
 }
