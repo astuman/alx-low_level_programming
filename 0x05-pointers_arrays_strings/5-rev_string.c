8c59d4163653435691f1b3da2b080093452f49ae
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-18 11:22:35 +0300

    commit task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * rev_string - prints a reverse string
+ *@s: A pointer to an int that will be changed
+ *
+ *Return: void
+ */
+void rev_string(char *s)
+{
+char s[10] = "My School";
+
+    printf("%s\n", s);
+    rev_string(s);
+    printf("%s\n", s);
+    return (0);
+}
