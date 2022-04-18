958423c729ec2f7a9dbcba047f9ecab7fca5269b
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-18 11:24:54 +0300

    commit task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,10 +7,10 @@
  */
 void rev_string(char *s)
 {
-char s[10] = "My School";
+char *s[10] = "My School";
 
-    printf("%s\n", s);
-    rev_string(s);
-    printf("%s\n", s);
+    printf("%s\n", *s);
+    rev_string(*s);
+    printf("%s\n", *s);
     return (0);
 }
