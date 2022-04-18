98edea8d2c41bb95aea0d566a9d1723cd7db5d17
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-18 12:57:30 +0300

    commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,13 +1,15 @@
+#include "main.c"
+#include "stdio.h"
 int main(void)
 {
-unsigned long count, i, k;
+unsigned long count, i, j; k;
 i = 0;
 j = 1;
 for (count = 0; count < 50; count++)
 {
 k = i+j;
 i = j;
-printf("%lu", k)
+printf("%lu", k);
 if (count == 49)
 {
 putchar("\n");
