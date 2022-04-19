84af2ce9f5020cccb25ff4a763f412d6372257db
astuman <101445231+astuman@users.noreply.github.com>
2022-04-19 13:13:11 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,24 +20,26 @@ _putchar(", ");
 _putchar(" ");
 }
 if(res < 10 && j != 0)
-_putchar(" ");
-_putchar(" ");
-_putchar(res % 10 + '0');
+putchar(" ");
+putchar(" ");
+putchar(res % 10 + '0');
 }
 else if(res > 10 && res < 100)
 {
-_putchar(' ');
-_putchar((res / 10) + '0');
-_putchar((res % 10) + '0');
+putchar(' ');
+putchar((res / 10) + '0');
+putchar((res % 10) + '0');
 }
 else if(res > 100 && j != 0)
 {
-_putchar((res / 100) + '0');
-_putchar((res / 10) % 10 + '0');
-_putchar((res % 10) + '0');
+putchar((res / 100) + '0');
+putchar((res / 10) % 10 + '0');
+putchar((res % 10) + '0');
 }
 else
+{
 _putchar((res % 10) + '0');
+}
 }
 _putchar('\n');
 }
