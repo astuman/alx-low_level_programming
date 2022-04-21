8ffcc70dbf74ffb9c22126c7fb71daccd128a6aa
astuman <101445231+astuman@users.noreply.github.com>
2022-04-21 15:42:52 +0300

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,18 +5,17 @@
  *@src: A pointer to a character that will also be changed
  *Return: dest
  */
-
 char *_strcat(char *dest, char *src)
 {
 int i, j;
-
 i = 0;
+
 while (dest[i] != '\0')
 {
 i++;
 }
-
 j = 0;
+  
 while (src[j] != '\0')
 {
 dest[i] = src[j];
