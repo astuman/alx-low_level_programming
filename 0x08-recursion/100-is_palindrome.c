e5d3e4f241093503ada28a353621e17034635162
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-27 16:35:56 +0300

    commit

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  *_strlen_recursion - size
  *@s: pointer to string params
@@ -6,17 +7,18 @@
  **/
 int _strlen_recursion(char *s)
 {
-if (!*s
+if (!*s)
 {
 return (0);
 }
 return (1 + _strlen_recursion(++s));
+}
 /**
  *p1 - palindrome
  *@s: pointer to string
  *@l: position
  *Return: boolena
- **/
+**/
 int p1(char *s, int l)
 {
 if (l < 1)
@@ -29,16 +31,13 @@ return (p1(s + 1, l - 2));
 }
 return (0);
 }
-
 /**
 *is_palindrome - palindrome
 *@s: pointer to string
 *Return: recursion
 **/
-
 int is_palindrome(char *s)
 {
-		int len = _strlen_recursion(s);
-
-			return (p1(s, len - 1));
+int len = _strlen_recursion(s);
+return (p1(s, len - 1));
 }
