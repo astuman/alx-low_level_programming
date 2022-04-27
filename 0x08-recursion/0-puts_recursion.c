61baacaa6dd9177f1838236bdf2cbb24edd7fcf4
astewul alemu derseh <loloalex1992@gmail.com>
2022-04-27 12:19:49 +0300

    my task commit

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,12 +1,18 @@
 #include "main.h"
 /**
- * main - dsds
- * return 0: ds
- */
-void _puts_recursion(char *s);
-{
-printf("welcome to c\n");
-	return (0);
+ *  *_puts_recursion - prints a string
+ *   *@s: pointer block of memory to fill
+ *    *Return: void
+ *     */
+void _puts_recursion(char *s)
 
+{
+if (*s == '\0')
+{
+_putchar('\n');
+return;
+}
+_putchar(*s);
+_putchar_recursion(s + 1);
 }
 
