f31ea98ee38f8da29262d90548d242e519f8cd44
Astewul <loloalex1992@gmail.com>
2022-05-02 18:23:17 +0300

    commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -6,7 +6,6 @@
 /**
  *check_num - check - string there are digit
  *@str: array str
- *
  *Return; Always 0 (Success)
  **/
 
@@ -16,9 +15,11 @@ int check_num(char *str)
 unsigned int count;
 count = 0;
 
-while (count < strlen(str)) /*count string*/
+while (count < strlen(str))
+/*count string*/
 {
-if (!isdigit(str[count])) /*check if str there are digit*/
+if (!isdigit(str[count]))
+/*check if str there are digit*/
 {
 return (0);
 }
@@ -41,11 +42,13 @@ int str_to_int;
 int sum = 0;
 count = 1;
 
-while (count < argc) /*Goes through the whole array*/
+while (count < argc)
+/*Goes through the whole array*/
 {
 if(check_num(argv[count]))
 {
-str_to_int = atoi(argv[count]); /*ATOI --> convert string to int*/
+str_to_int = atoi(argv[count]);
+/*ATOI --> convert string to int*/
 sum += str_to_int;
 }
 /*Condition if one of the number contains symbols that are not digits*/
