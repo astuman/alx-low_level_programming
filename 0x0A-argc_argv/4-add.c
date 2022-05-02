ba4c8b25f1b008b356e44b784937acbc8ef4c55d
Astewul <loloalex1992@gmail.com>
2022-05-02 18:03:37 +0300

    commit

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,12 +11,11 @@
  **/
 
 int check_num(char *str)
-
 {
 /*Declaring variables*/
 unsigned int count;
-
 count = 0;
+
 while (count < strlen(str)) /*count string*/
 {
 if (!isdigit(str[count])) /*check if str there are digit*/
@@ -37,15 +36,13 @@ return (1);
  **/
 
 int main(int argc, char *argv[])
-
 {
-
 /*Declaring variables*/
 int count;
 int str_to_int;
 int sum = 0;
-
 count = 1;
+
 while (count < argc) /*Goes through the whole array*/
 {
 if(check_num(argv[count]))
@@ -64,4 +61,3 @@ count++;
 printf("%d\n", sum); /*print sum*/
 return (0);
 }
-
