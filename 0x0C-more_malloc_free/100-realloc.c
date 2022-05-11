1902b50b009e1310d2084712e45f2c394a4f00e2
Astewul <loloalex1992@gmail.com>
2022-05-11 15:52:02 +0300

    my last commits

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -7,12 +7,14 @@
  *@new_size: new size
  *Return: pointer
  **/
+
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 char *clone, *relloc;
 unsigned int i;
 if (ptr != NULL)
 clone = ptr;
+
 else
 { return (malloc(new_size)); }
 if (new_size == old_size)
@@ -26,8 +28,8 @@ return (0);
 for (i = 0; i < (old_size || i < new_size); i++)
 {
 *(relloc + i) = clone[i];
+
 }
 free(ptr);
 return (relloc);
 }
-
