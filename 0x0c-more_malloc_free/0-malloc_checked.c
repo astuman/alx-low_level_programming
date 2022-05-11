1885ce6e2cfbcc61ef5ac703a3b3d81d96c6abaf
astewul alemu derseh <loloalex1992@gmail.com>
2022-05-11 12:26:23 +0300

    commit

--- 0x0c-more_malloc_free/0-malloc_checked.c
+++ 0x0c-more_malloc_free/0-malloc_checked.c
@@ -4,13 +4,17 @@ nclude "main.h"
  *@b: number of memory
  *Return: void
  **/
+
 void *malloc_checked(unsigned int b)
 {
 void *p;
+
 p = malloc(b);
+
 if (p == NULL)
 {
 exit(98);
 }
+
 return (p);
 }
