3ccd0c033f224af90962888ef811533b4eaacd4c
Astewul <loloalex1992@gmail.com>
2022-05-11 18:38:48 +0300

    commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -10,9 +10,12 @@ int main(int argc, char *argv[])
 {
 unsigned long mul;
 int i, j;
+
 if (argc != 3)
-{ printf("Error\n");
-exit(98); }
+{
+printf("Error\n");
+exit(98);
+}
 for (i = 1; i < argc; i++)
 {
 for (j = 0; argv[i][j] != '\0'; j++)
@@ -25,6 +28,7 @@ exit(98);
 }
 }
 mul = atol(argv[1]) *atol(argv[2]);
+
 printf("%lu\n", mul);
 return (0);
 }
