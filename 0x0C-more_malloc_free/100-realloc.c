96256f0cea157995b60c7c5b1805efc9b61f3c82
Astewul <loloalex1992@gmail.com>
2022-05-11 15:59:08 +0300

    my last commits

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -18,7 +18,8 @@ clone = ptr;
 
 else
 {
-return (malloc(new_size)); }
+return (malloc(new_size));
+}
 if (new_size == old_size)
 return (ptr);
 if (new_size == 0 && ptr != NULL)
@@ -34,6 +35,5 @@ for (i = 0; i < (old_size || i < new_size); i++)
 *(relloc + i) = clone[i];
 }
 free(ptr);
-
 return (relloc);
 }
