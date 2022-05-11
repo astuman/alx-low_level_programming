2adccad34845c692f6f91d2ab209911614b2987c
Astewul <loloalex1992@gmail.com>
2022-05-11 15:49:03 +0300

    my last commits

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  *main - multiplies two positive numbers
--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+#include "main.h"
 
 /**
  *array_range - creates an array of integers
