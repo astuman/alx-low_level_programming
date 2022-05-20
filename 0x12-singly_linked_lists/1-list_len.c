a66775b710c87d8761650d849f0b5083f65ca346
Astewul <loloalex1992@gmail.com>
2022-05-20 22:57:34 +0300

    commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,16 +1,19 @@
 #include "lists.h"
 #include "stdio.h"
 /**
- * ToIntFunction: holds number of nodes
+ *print_list - prints all the elements
+ * @h: pointer to the list_t list to point
  * Return: the number of nodes printed
  **/
+
 size_t list_len(const list_t *h);
 {
 size_t size = 0;
 while(h) 
 {
 if(!h->str)
-_putchar("[0], (nil)");
+_putchar("[0]");
+_putchar("(nill)");
 else
 _putchar(h->len);
 _putchar(h->str);
