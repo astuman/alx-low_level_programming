aed2a0c460461bf550a78717e1dcef8c8fa66546
Astewul <loloalex1992@gmail.com>
2022-05-20 23:33:26 +0300

    commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *print_list - prints all the elements
+ *list_len - prints all the elements
  * @h: pointer to the list_t list to point
  * Return: the number of nodes printed
  **/
