b3eb636c998e8e06479281db2138d4060a659bfe
Astewul <loloalex1992@gmail.com>
2022-05-20 23:30:32 +0300

    commit

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -8,15 +8,11 @@
 
 size_t list_len(const list_t *h);
 {
-size_t size = 0;
+size_t nnn = 0;
 while(h) 
 {
-if(!h->str)
-printf("[0], (nil)\n");
-else
-printf("[%u] %s\n", h->len, h->str);
+nnn++;
 h = h->next;
-size++;
 }
-return(size);
+return(nnn);
 };
