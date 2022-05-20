6c45109775466eeb983fce0068a5d8fed37186a5
Astewul <loloalex1992@gmail.com>
2022-05-20 23:23:37 +0300

    commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -6,14 +6,13 @@
  **/
 size_t print_list(const list_t *h)
 {
-size_t aa;
-aa = 0;
+size_t aa = 0;
 while (h)
 {
 if (!h->str)
 printf("[0] (nil)\n");
 else
-printf("[0] %s\n", h->len, h->str);
+printf("[%u] %s\n", h->len,h->str);
 h = h->next;
 aa++;
 }
