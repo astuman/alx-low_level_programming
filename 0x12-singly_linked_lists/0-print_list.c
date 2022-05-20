c8e9f5bcba8375df4dca20c6911cda2ce1a4e114
astewul alemu derseh <loloalex1992@gmail.com>
2022-05-20 12:36:57 +0300

    my first commit

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_list - prints all the elements
+ * @h: singly linked list
+ * Return: number of elements in the list
+ **/
+size_t print_list(const list_t *h)
+{
+size_t as;
+as = 0;
+while ( h != NULL)
+{
+if (h->str == NULL)
+printf("[%d] %s\n", h->len, h->str);
+h = h->next;
+as++;
+}
+return (as);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Thi is my readme about 0x12. C - Singly linked lists
