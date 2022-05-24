30383d8d1172a4e259986bc06c5b27b0c99c9051
Astewul <loloalex1992@gmail.com>
2022-05-24 12:44:46 +0300

    my commit is here

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,4 +1,5 @@
 #include "lists.h"
+
 /**
  *add_nodeint_end - adds a node at the end of a linked list
  *@head: points to the first element in the list
@@ -9,6 +10,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 listint_t *new;
 listint_t *temp = *head;
+
 new = malloc(sizeof(listint_t));
 if(!new)
 return (NULL);
@@ -21,6 +23,7 @@ if(*head == NULL)
 return (new);
 }
 while(temp->next)
+temp->next = new;
 
 temp->next = new;
 
