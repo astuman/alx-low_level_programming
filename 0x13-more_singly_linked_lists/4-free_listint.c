3a03dc8c5fc5d11d3aee4e6c0cf8037e4cf3aaf4
Astewul <loloalex1992@gmail.com>
2022-05-24 15:26:02 +0300

    commit

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,15 +1,16 @@
 #include "lists.h"
+
 /**
  *free_listint - frees a linked list
  *@head: lists to be freed
  **/
 void free_listint(listint_t *head)
 {
-	listint_t *tmp;
-	while (head)
+listint_t *tmp;
+while (head)
 {
-	temp = head->next;
-	free(head);
-	head = temp
+temp = head->next;
+free(head);
+head = temp
 }
 }
