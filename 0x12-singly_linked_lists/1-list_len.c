8e85f1ddcff1bbd804c32a62fffa4fb8c5b1c9cf
Astewul <loloalex1992@gmail.com>
2022-05-27 18:28:07 +0300

    commitinf

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,11 +1,10 @@
 #include "lists.h"
 
 /**
- *list_len - prints all the elements in a linked list
- *@h: pointer to the list_t list to point
- *Return: number of elements in h
+ *list_len - returns then number of elements in a list.
+ *@h: singly linked list.
+ *Return: number of elements in the list.
  **/
-
 size_t list_len(const list_t *h)
 {
 size_t as = 0;
