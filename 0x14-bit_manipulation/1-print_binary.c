1d957079b4843a5b341d518a88735943e97d1971
Astewul <loloalex1992@gmail.com>
2022-05-27 16:15:14 +0300

    commit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,13 +1,16 @@
 #include "main.h"
+
 /**
  *print_binary - printing a binary number
  *@n: hold unsigned int
  *Return: return a value
- */
+ **/
 void print_binary(unsigned long int n);
 {
 int i, cc = 0;
+
 unsigned long int current;
+
 for(x = 63; x >= 0; x--)
 {
 current = n >> x;
@@ -24,4 +27,5 @@ _putchar('0');
 }
 if (!cc)
 _putchar('0');
+
 }
