b1510132ec0176606f050db821bd4a3c917c83da
Astewul <loloalex1992@gmail.com>
2022-05-27 18:43:38 +0300

    commit

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,10 +6,12 @@
  *@str: string to store in the list.
  *Return: address of the head.
  **/
+
 list_t *add_node(list_t **head, const char *str)
 {
 list_t *new;
 size_t nchar;
+
 new = malloc(sizeof(list_t));
 
 if (new == NULL)
@@ -17,9 +19,10 @@ return (NULL);
 new->str = strdup(str);
 
 for (nchar = 0; str[nchar]; nchar++)
+{
 new->len = nchar;
 new->next = *head;
 *head = new;
-
+}
 return (*head);
 }
