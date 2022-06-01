8dfc79426e462e9e5bd50958461025ea9d154d88
Astewul <loloalex1992@gmail.com>
2022-06-01 18:29:56 +0300

    commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,17 +12,16 @@ int create_file(const char *filename, char *text_content)
 int file, fwrite, x;
 
 if (filename == NULL)
-	return (-1);
+return (-1);
 file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 if (file == -1)
-	return (-1);
+return (-1);
 if (text_content != NULL)
 {
-for (x = 0; text_content[i]; x++)
-
+for (x = 0; text_content[x]; x++)
 fwrite = write(file, text_content, x);
 if (fwrite == -1)
-	return (-1);
+return (-1);
 }
 close(file);
 
