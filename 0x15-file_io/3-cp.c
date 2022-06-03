1e6f64692dedfcd33b451ee94edbfe898678dbfd
Astewul <loloalex1992@gmail.com>
2022-06-03 16:52:49 +0300

    commiting

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,6 +10,7 @@
 int main(int argc, char **argv)
 {
 int f1, f2, n;
+
 char buf[1024];
 if(argc != 3)
 {
@@ -17,12 +18,14 @@ dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
 f1 = open(argv[1], O_RDONLY);
+
 if(f1 == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
 f2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+
 while ((n = read(f1, buf, 1024)) > 0)
 {
 if(write(f2, buf, n) != n || f2 == -1)
