177097f4a84256d65f33d2fe9f1742cb1c3d63ad
Astewul <loloalex1992@gmail.com>
2022-06-03 16:34:37 +0300

    commit

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -6,28 +6,27 @@
  *@argv: double pointer
  *Return: the actual number of letters it could read and print
  **/
-
 int main(int argc, char **argv)
 {
-int file1, file2, n;
+int f1, f2, n;
 char buf[1024];
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-file1 = open(argv[1], O_RDONLY);
-if (file1 == -1)
+f1 = open(argv[1], O_RDONLY);
+if (f1 == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-file2 = open(argv[2], O_CREAT | O_TRUNC | O_RDONLY, 0664);
-while ((n = read(file1, buf, 1024)) > 0)
+f2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+while ((n = read(f1, buf, 1024)) > 0)
 {
-if (write(file1, buf, n) != n || file2 == -1)
+if (write(f2, buf, n) != n || f2 == -1)
 {
-dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+     dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 exit(99);
 }
 }
@@ -36,14 +35,14 @@ if (n == -1)
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 exit(98);
 }
-if (close(file1 < 0))
+if (close(f1) < 0)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1);
 exit(100);
 }
-if (close(file2) < 0)
+if (close(f2) < 0)
 {
-dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
+dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2);
 exit(100);
 }
 return (0);
